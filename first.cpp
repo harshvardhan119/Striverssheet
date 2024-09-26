733e07bbf8331949bf1941b838f27b964ec24315
Harshvardhansingh19 <yobrobiru@gmail.com>
2024-09-26 18:46:42 +0530

    first day

---
+++ first.cpp
@@ -0,0 +1,98 @@
+// input output 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout<<"Harsh Dsa Starting"<<endl;
+   string a;
+   
+   cout<<"enter your name:";
+   cin>>a;
+   cout<<"the name entered is: "<<a<<"\n";
+   //to enter two number at a time
+   cout<<"enter both number with space between them:";
+   int c;
+   int b;
+   cin>>c>>b;
+   cout<<"numbers are :"<<c<<" and "<<b;
+   return 0;
+
+    
+}
+//Datatypes
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hi' lets start datatype"<<endl;
+    int a;//4 byte
+    long b;//8 byte
+    long long c;//8 byte
+    string d;//
+    char e;//1 byte
+    getline(cin,d);
+    cout<<d;
+    return 0;
+}
+//if-else statement
+// These condition can go nested too
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter your age: ";
+    cin>>a;
+    if(a>18){
+        cout<<"eligible for voting";
+    }
+    else if(a<18){
+        cout<<"Not eligible for Voting";
+    }
+    else{
+        cout<<"Go create voting ID";
+    }
+    
+   return 0;
+   
+}
+//Switch Statement
+#include<iostream>
+    using namespace std;
+    int main(){
+    int a;
+    cout<<"enter the number of day you want to print:";
+    cin>>a;
+    switch(a){
+        case 1:
+        cout<<"monday";
+        break;
+        case 2:
+        cout<<"Tuesday";
+        break;
+        case 3:
+        cout<<"Wednesday";
+        break;
+        case 4:
+        cout<<"Thrusday";
+        break;
+        case 5:
+        cout<<"Friday";
+        break;
+        case 6:
+        cout<<"Staurday";
+        break;
+        case 7:
+        cout<<"Sunday best day";
+        break;
+        default:
+        cout<<"Not yet Defined";
+        break;
+    }
+    
+    
+    
+    return 0;
+}
+//Basic of Array and Strings
